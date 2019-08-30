ce672baea4b6063f39631c4709ac2a1c414427bf
Carlos Ruiz <zimoo354@gmail.com>
2019-08-30 17:02:47 -0500

    Removed DB credentials LOL

--- config/db.php
+++ config/db.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn = mysqli_connect("127.0.0.1", "ztekcomm_atextou", "Atexto123!", "ztekcomm_atexto") or die("Connection failed: " . mysqli_connect_error());
+$conn = mysqli_connect("127.0.0.1", "HERE_GOES_YOUR_USER", "HERE_GOES_YOUR_PASSWORD", "HERE_GOES_YOUR_DB_NAME") or die("Connection failed: " . mysqli_connect_error());
